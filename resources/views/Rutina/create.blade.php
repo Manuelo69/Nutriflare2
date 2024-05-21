1c4560f9d885b2ab3a12501c6adce0b7873e52f0
manuelo69 <ichigo2418@gmail.com>
2024-05-21 16:53:25 +0200

    Contenedor responsivo, queda añadir que se guarde. pero funcionalidad principal lograda

--- resources/views/Rutina/create.blade.php
+++ resources/views/Rutina/create.blade.php
@@ -1,4 +1,3 @@
-{{-- resources/views/rutina/create.blade.php --}}
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -17,50 +16,99 @@
         </div>
     @endif
 
-    <div class="py-12 flex gap-4">
-        <div class="mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5 h-screen">
-                <div class="flex flex-col gap-4">
-                    <div class="flex flex-row gap-4 mb-4">
-                        <input type="text" id="nombre" placeholder="Nombre del ejercicio" class="border p-2">
-                        <select id="musculo" class="border p-2">
-                            <option value="">Todos los músculos</option>
-                            <option value="pierna">Pierna</option>
-                            <option value="triceps">Tríceps</option>
-                            <option value="biceps">Bíceps</option>
-                            <option value="pecho">Pecho</option>
-                            <option value="espalda">Espalda</option>
-                            <option value="abdominales">Abdominales</option>
-                            <option value="hombro">Hombro</option>
-                        </select>
-                        <button onclick="filtrarEjercicios()"
-                            class="bg-blue-500 text-white rounded px-4 py-2">Filtrar</button>
+    <div class="py-12 flex flex-col lg:flex-row gap-4">
+        <div class="w-full lg:w-2/3 mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5 h-full">
+                <!-- Filtros -->
+                <div class="flex flex-col items-center mb-4">
+                    <input type="text" id="filtroNombre" class="border p-2 m-2 w-full"
+                        placeholder="Nombre del ejercicio">
+                    <select id="filtroMusculo" class="border p-2 m-2 w-full">
+                        <option value="">Todos los músculos</option>
+                        <option value="pierna">Pierna</option>
+                        <option value="triceps">Tríceps</option>
+                        <option value="biceps">Bíceps</option>
+                        <option value="pecho">Pecho</option>
+                        <option value="espalda">Espalda</option>
+                        <option value="abdominales">Abdominales</option>
+                        <option value="hombro">Hombro</option>
+                    </select>
+                    <div class="flex gap-2">
+                        <button id="btnFiltrar" class="bg-blue-500 text-white px-4 py-2 rounded">Filtrar</button>
+                        <button id="btnReset" class="bg-gray-500 text-white px-4 py-2 rounded">Reset</button>
                     </div>
+                </div>
 
-                    <div id="ejercicios-container" class="grid grid-cols-3 gap-4">
-                        @include('rutina._lista_ejercicios', ['ejercicios' => $ejercicios])
-                    </div>
+                <!-- Lista de ejercicios -->
+                <div id="ejercicios-container"
+                    class="overflow-y-auto h-[calc(100vh-300px)] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    @include('rutina._lista_ejercicios', ['ejercicios' => $ejercicios])
+                </div>
 
-                    <div id="paginacion-container" class="mt-4">
-                        {{ $ejercicios->links() }}
-                    </div>
+                <!-- Paginación -->
+                <div id="paginacion-container" class="mt-4">
+                    {{ $ejercicios->links() }}
                 </div>
             </div>
         </div>
 
-        <div class="flex flex-col w-1/3 bg-gray-100 p-4 rounded shadow">
-            <h3 class="text-lg font-semibold mb-2">Ejercicios Añadidos</h3>
-            <div id="ejercicios-añadidos-container" class="grid grid-cols-3 gap-4"></div>
-            <button onclick="guardarRutina()" class="bg-green-500 text-white rounded px-4 py-2 mt-4">Guardar
-                Rutina</button>
+        <div class="w-full lg:w-1/3">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5 h-full flex flex-col">
+                <!-- Selección del día de la semana -->
+                <div class="mb-4">
+                    <label for="diaSemana" class="block text-sm font-medium text-gray-700">Día de la semana</label>
+                    <select id="diaSemana" class="border p-2 mt-1 w-full">
+                        <option value="Lunes">Lunes</option>
+                        <option value="Martes">Martes</option>
+                        <option value="Miércoles">Miércoles</option>
+                        <option value="Jueves">Jueves</option>
+                        <option value="Viernes">Viernes</option>
+                        <option value="Sábado">Sábado</option>
+                        <option value="Domingo">Domingo</option>
+                    </select>
+                </div>
+                <!-- Previsualización de la rutina -->
+                <h3 class="text-xl font-semibold mb-4">Ejercicios Añadidos</h3>
+                <div id="ejercicios-añadidos-container" class="flex flex-wrap gap-4 overflow-y-auto items-center"></div>
+                <button onclick="guardarRutina()" class="bg-green-500 text-white rounded px-4 py-2 mt-4">Guardar
+                    Rutina</button>
+            </div>
         </div>
     </div>
 
-    <!-- Modal para mostrar los detalles del ejercicio -->
-    <div id="modal-detalles" class="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center hidden">
-        <div class="bg-white p-4 rounded shadow w-1/2">
-            <button onclick="cerrarModal()" class="text-red-500 float-right">Cerrar</button>
-            <div id="detalles-ejercicio"></div>
+    <!-- Modal de detalles del ejercicio -->
+    <div id="modalDetalles" class="fixed z-10 inset-0 overflow-y-auto hidden">
+        <div class="flex items-center justify-center min-h-screen px-4 text-center">
+            <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+                <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+            </div>
+            <div
+                class="inline-block bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
+                <div class="bg-white p-6">
+                    <div id="modalImagen" class="mb-4"></div>
+                    <h3 class="text-xl font-medium leading-6 text-gray-900" id="modalNombre"></h3>
+                    <div id="modalExplicacion" class="mt-2 text-sm text-gray-500"></div>
+                    <button id="cerrarModal" class="mt-4 bg-red-500 text-white px-4 py-2 rounded">Cerrar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal de advertencia -->
+    <div id="modalAdvertencia" class="fixed z-10 inset-0 overflow-y-auto hidden">
+        <div class="flex items-center justify-center min-h-screen px-4 text-center">
+            <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+                <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+            </div>
+            <div
+                class="inline-block bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
+                <div class="bg-white p-6">
+                    <h3 class="text-xl font-medium leading-6 text-gray-900">Advertencia</h3>
+                    <div class="mt-2 text-sm text-gray-500">No puedes añadir más de 12 ejercicios a la rutina.</div>
+                    <button id="cerrarModalAdvertencia"
+                        class="mt-4 bg-red-500 text-white px-4 py-2 rounded">Cerrar</button>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -70,14 +118,15 @@
         });
 
         function filtrarEjercicios(page = 1) {
-            const nombre = document.getElementById('nombre').value;
-            const musculo = document.getElementById('musculo').value;
+            const nombre = document.getElementById('filtroNombre').value;
+            const musculo = document.getElementById('filtroMusculo').value;
 
             fetch(`{{ route('rutina.filtrar') }}?nombre=${nombre}&musculo=${musculo}&page=${page}`)
                 .then(response => response.json())
                 .then(data => {
                     document.getElementById('ejercicios-container').innerHTML = data.html;
                     document.getElementById('paginacion-container').innerHTML = data.paginacion;
+                    activarPaginacion();
                 });
         }
 
@@ -85,70 +134,120 @@
             fetch(`/ejercicio/${id}`)
                 .then(response => response.json())
                 .then(data => {
-                    const detalles = `
-                        <h3 class="text-lg font-semibold">${data.nombre_ejercicio}</h3>
-                        <p>${data.explicacion}</p>
-                    `;
-                    document.getElementById('detalles-ejercicio').innerHTML = detalles;
-                    document.getElementById('modal-detalles').classList.remove('hidden');
+                    document.getElementById('modalNombre').textContent = data.nombre_ejercicio;
+                    document.getElementById('modalExplicacion').textContent = data.explicacion;
+                    document.getElementById('modalImagen').innerHTML =
+                        `<img src="${data.imagen}" alt="${data.nombre_ejercicio}" class="mx-auto">`;
+                    document.getElementById('modalDetalles').classList.remove('hidden');
                 });
         }
 
         function cerrarModal() {
-            document.getElementById('modal-detalles').classList.add('hidden');
+            document.getElementById('modalDetalles').classList.add('hidden');
+        }
+
+        function cerrarModalAdvertencia() {
+            document.getElementById('modalAdvertencia').classList.add('hidden');
         }
 
         function añadirEjercicio(id, nombre, musculo) {
             const ejerciciosAñadidosContainer = document.getElementById('ejercicios-añadidos-container');
+            if (ejerciciosAñadidosContainer.children.length >= 12) {
+                document.getElementById('modalAdvertencia').classList.remove('hidden');
+                return;
+            }
             const ejercicioDiv = document.createElement('div');
-            ejercicioDiv.classList.add('flex', 'flex-col', 'items-center', 'border', 'p-2');
+            ejercicioDiv.classList.add('border', 'p-2', 'm-2', 'flex', 'flex-col', 'items-center', 'w-full', 'lg:w-1/4',
+                'box-border');
             ejercicioDiv.setAttribute('data-id', id);
             ejercicioDiv.innerHTML = `
-                <h3 class="text-lg font-semibold truncate">${nombre}</h3>
+                <h3 class="text-lg font-semibold">${nombre}</h3>
                 <p class="text-gray-500">${musculo}</p>
-                <input type="number" placeholder="Series" class="border p-1 mt-2">
-                <input type="number" placeholder="Repeticiones" class="border p-1 mt-2">
-                <button onclick="eliminarEjercicio(this)" class="bg-red-500 text-white rounded px-2 py-1 mt-2">Eliminar</button>
+                <input type="number" class="border p-1 mt-2 w-full" placeholder="Series" min="1">
+                <input type="number" class="border p-1 mt-2 w-full" placeholder="Repeticiones" min="1">
+                <button onclick="eliminarEjercicio(${id})" class="bg-red-500 text-white rounded px-2 py-1 mt-2">Eliminar</button>
             `;
             ejerciciosAñadidosContainer.appendChild(ejercicioDiv);
         }
 
-        function eliminarEjercicio(button) {
-            const ejercicioDiv = button.parentNode;
-            ejercicioDiv.remove();
+        function eliminarEjercicio(id) {
+            const ejerciciosAñadidosContainer = document.getElementById('ejercicios-añadidos-container');
+            const ejercicioDiv = ejerciciosAñadidosContainer.querySelector(`[data-id="${id}"]`);
+            ejerciciosAñadidosContainer.removeChild(ejercicioDiv);
         }
 
         function guardarRutina() {
+            const diaSemana = document.getElementById('diaSemana').value;
+            const ejerciciosAñadidosContainer = document.getElementById('ejercicios-añadidos-container');
             const ejercicios = [];
-            const ejercicioDivs = document.querySelectorAll('#ejercicios-añadidos-container > div');
 
-            ejercicioDivs.forEach(ejercicioDiv => {
+            for (let ejercicioDiv of ejerciciosAñadidosContainer.children) {
                 const id = ejercicioDiv.getAttribute('data-id');
                 const series = ejercicioDiv.querySelector('input[placeholder="Series"]').value;
                 const repeticiones = ejercicioDiv.querySelector('input[placeholder="Repeticiones"]').value;
+                if (!series || !repeticiones) {
+                    alert('Debes ingresar las series y repeticiones para todos los ejercicios.');
+                    return;
+                }
                 ejercicios.push({
                     id,
                     series,
                     repeticiones
                 });
-            });
+            }
 
-            fetch('{{ route('rutina.store') }}', {
+            fetch(`{{ route('rutina.store') }}`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
                         'X-CSRF-TOKEN': '{{ csrf_token() }}'
                     },
                     body: JSON.stringify({
+                        dia: diaSemana,
                         ejercicios
                     })
                 })
-                .then(response => response.json())
-                .then(data => {
-                    if (data.success) {
-                        alert('Rutina guardada con éxito.');
+                .then(response => {
+                    if (response.ok) {
+                        window.location.href = '{{ route('dashboard') }}';
+                    } else {
+                        alert('Error al guardar la rutina.');
                     }
                 });
+        }
+
+        document.getElementById('btnFiltrar').addEventListener('click', function() {
+            filtrarEjercicios();
+        });
+
+        document.getElementById('btnReset').addEventListener('click', function() {
+            resetFiltros();
+        });
+
+        document.getElementById('cerrarModal').addEventListener('click', function() {
+            cerrarModal();
+        });
+
+        document.getElementById('cerrarModalAdvertencia').addEventListener('click', function() {
+            cerrarModalAdvertencia();
+        });
+
+        function resetFiltros() {
+            document.getElementById('filtroNombre').value = '';
+            document.getElementById('filtroMusculo').selectedIndex = 0;
+            filtrarEjercicios();
+        }
+
+        function activarPaginacion() {
+            const paginacionLinks = document.querySelectorAll('#paginacion-container a');
+            paginacionLinks.forEach(link => {
+                link.addEventListener('click', function(e) {
+                    e.preventDefault();
+                    const url = new URL(link.href);
+                    const page = url.searchParams.get('page');
+                    filtrarEjercicios(page);
+                });
+            });
         }
     </script>
 </x-app-layout>
